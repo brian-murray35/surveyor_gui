cb77344a06032421d3ffb586891c5eca1c5e159f
brian-murray35 <brian.murray@sap.com>
2016-11-23 22:19:15 +0000

    Update surveyforms_controller.rb

--- app/controllers/surveyor_gui/surveyforms_controller.rb
+++ app/controllers/surveyor_gui/surveyforms_controller.rb
@@ -1,4 +1,7 @@
 class SurveyorGui::SurveyformsController < ApplicationController
   include Surveyor::SurveyorControllerMethods
   include SurveyorGui::SurveyformsControllerMethods
+  def update
+  redirect_to "plays/index"
+end
 end
